9c2b56cc8c52b1018c12f3b4122141840dbc86d5
venky <venky@LAPTOP-RMVPP3IL>
2021-05-16 19:49:18 +0530

    iii

--- Lamboks/src/main/java/com/example/demo/controller/ControllerClass.java
+++ Lamboks/src/main/java/com/example/demo/controller/ControllerClass.java
@@ -12,7 +12,7 @@ public class ControllerClass {
 
 	@GetMapping("/hello")
 	public String method() {
-		return "hello-world-hello-two-twooo";
+		return "hello-world-hello-two-twoooiii";
 	}
 	ArrayList<ModelClass> al=new ArrayList<>();
 	
