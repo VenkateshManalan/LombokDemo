5d6543b93e8bf074cd96ffe4c12cc5a0cb7d1f0e
venky <venky@LAPTOP-RMVPP3IL>
2021-05-16 19:25:05 +0530

    first commit

---
+++ Lamboks/src/main/java/com/example/demo/controller/ControllerClass.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.ModelClass;
+
+@RestController
+public class ControllerClass {
+
+	@GetMapping("/hello")
+	public String method() {
+		return "hello";
+	}
+	ArrayList<ModelClass> al=new ArrayList<>();
+	
+	@GetMapping("/data")
+	public void method2()
+	{
+		ModelClass m=new ModelClass();
+		m.setName("kumar");
+		al.add(m);
+		
+	}
+	@GetMapping("/dataout")
+	public String method3()
+	{
+	 return al.get(0).getName();
+		
+	}
+
+}
