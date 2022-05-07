c41daf55735c2f9612cd3de28d643f8d8aade2e4
VenkateshManalan <84274516+VenkateshManalan@users.noreply.github.com>
2022-05-07 19:38:11 +0530

    Revert "test2"

--- Demo/src/FirstClass.java
+++ Demo/src/FirstClass.java
@@ -2,8 +2,9 @@
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.print("heloootest2");
+		System.out.print("helooo");
 		System.out.print("gittest1");
+		
 
 	}
 
